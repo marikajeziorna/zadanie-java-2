c384214b77b8b4c692128197ea4df645b494c5c5
Marika Jeziorna <marika.jeziorna@collibra.com>
2019-01-15 10:12:50 +0100

    homework-zadanie-java-2: created constructor

--- src/Shop.java
+++ src/Shop.java
@@ -1,17 +1,12 @@
 public class Shop {
 	public static void main(String[] args) {
-		Table table1 = new Table();
-		table1.material = "wood";
-		table1.numberOfLegs = 1;
-		table1.shape = "circle";
+		Table table1 = new Table("wood", "circle", 1);
 
-		Table table2 = new Table();
-		table2.material = "glass and steel";
-		table2.numberOfLegs = 4;
-		table2.shape = "rectangle";
+		Table table2 = new Table("glass and steel", "rectangle", 4);
 
 		System.out.println(
-				"Specification for the table no.1: \n material: " + table1.material + ", number of legs: " + table1.numberOfLegs + " and table shape: ." + table1.shape);
-
+				"Specification for the table no.1: \n material: " + table1.material + ", number of legs: " + table1.numberOfLegs + " and table shape: " + table1.shape + ".");
+		System.out.println(
+				"Specification for the table no.1: \n material: " + table2.material + ", number of legs: " + table2.numberOfLegs + " and table shape: " + table2.shape + ".");
 	}
 }
--- src/Table.java
+++ src/Table.java
@@ -2,4 +2,10 @@ public class Table {
 	String material;
 	String shape;
 	int numberOfLegs;
+
+	Table(String material, String shape, int numberOfLegs) {
+		this.material = material;
+		this.shape = shape;
+		this.numberOfLegs = numberOfLegs;
+	}
 }
