b616cae6b260b449710da7d5841e8536a6ded035
Marika Jeziorna <marika.jeziorna@collibra.com>
2019-01-15 10:43:16 +0100

    homework-zadanie-java-2: refactor on the System.out.printLn

--- src/Shop.java
+++ src/Shop.java
@@ -1,8 +1,14 @@
 public class Shop {
 	public static void main(String[] args) {
-		Table table1 = new Table("wood", "circle", 1);
+		Table table1 = new Table();
+		table1.material = "wood";
+		table1.numberOfLegs = 1;
+		table1.shape = "circle";
 
-		Table table2 = new Table("glass and steel", "rectangle", 4);
+		Table table2 = new Table();
+		table2.material = "glass and steel";
+		table2.numberOfLegs = 4;
+		table2.shape = "rectangle";
 
 		System.out.println(
 				"Specification for the table no.1: \n material: " + table1.material + ", number of legs: " + table1.numberOfLegs + " and table shape: " + table1.shape + ".");
--- src/Table.java
+++ src/Table.java
@@ -2,10 +2,4 @@ public class Table {
 	String material;
 	String shape;
 	int numberOfLegs;
-
-	Table(String material, String shape, int numberOfLegs) {
-		this.material = material;
-		this.shape = shape;
-		this.numberOfLegs = numberOfLegs;
-	}
 }
